76eeaab80e4f1c26040684fcdd68648e265a7493
mac <hendriarga28@gmail.com>
2024-11-06 20:46:47 +0700

    Number

---
+++ number.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println("Pi: ", math.Pi)
+	fmt.Println("Uint8: ", 100)
+	fmt.Println("Int8: ", -100)
+	fmt.Println("Uint16: ", 1000)
+	fmt.Println("Int16: ", -1000)
+	fmt.Println("Uint32: ", 10000)
+	fmt.Println("Int32: ", -10000)
+	fmt.Println("Uint64: ", 100000)
+	fmt.Println("Int64: ", -100000)
+}
