2576aa20aaa7e56fab2f847e248f68a122facdcb
mac <hendriarga28@gmail.com>
2024-11-11 17:24:08 +0700

    Math

---
+++ math.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("My favorit number", rand.Intn(16))
+	fmt.Println("Square root of 16 is", math.Sqrt(16))
+	fmt.Println("Pi is", math.Pi)
+}
