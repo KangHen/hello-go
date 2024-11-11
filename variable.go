9bcc2411992e05675e343faf36a90c0c8509c2e2
mac <hendriarga28@gmail.com>
2024-11-11 17:37:35 +0700

    variable

---
+++ variable.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+var c, python, java bool
+var n, x = 1, 2
+
+func main() {
+	var i int
+	k := 3
+	g, h, j := true, false, "no!"
+
+	fmt.Println(i, c, python, java)
+	fmt.Println(n, x)
+	fmt.Println(g, h, j)
+	fmt.Println(k)
+
+}
